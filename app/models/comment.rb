575a45f1f75101bfd690006ca901c20bcc04c6b5
EC2 Default User <ec2-user@ip-172-31-9-176.ap-northeast-1.compute.internal>
2023-02-15 14:31:43 +0000

    改善1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   
+  validates :comment, presence: true
+  
   def self.search(search)
     if search
       Comment.where(['comment LIKE ?', "%#{search}%"])
