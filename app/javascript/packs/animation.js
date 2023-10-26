e8edc08288733f1388f0b48c438580013d3db6cb
EC2 Default User <ec2-user@ip-172-31-21-65.ap-northeast-1.compute.internal>
2023-10-26 15:52:19 +0000

    レイアウト調整

--- app/javascript/packs/animation.js
+++ app/javascript/packs/animation.js
@@ -1,19 +1,25 @@
 /*global $*/
 $(function() {
-  const CLASSNAME = "-visible";
+  const CLASSNAME = "visible1";
   const $target1 = $(".title");
 
   setInterval(() => {
     $target1.addClass(CLASSNAME);
   });
-});
 
-$(function() {
+
   $('.submit1').delay(2500).hide().fadeIn('slow');
-});
 
-$(function() {
   $('.box').hide().fadeIn('3000');
+
+  setTimeout(() => {
+    $('.title').addClass('title-height');
+    $('.top-tag .container').addClass(CLASSNAME);
+  }, 2500);
+  
+  setTimeout(() => {
+    $('.top-post .container').addClass(CLASSNAME);
+  }, 2700);
 });
 
 
